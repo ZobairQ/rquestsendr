37a3c57f0772588ae2b58bab4507a6d2c5759917
Zobair Qauomi <Zobairq55@gmail.com>
2021-01-10 20:20:24 +0100

    add a simple server that sends request to a website and renders it locally

--- main.go
+++ main.go
@@ -10,14 +10,16 @@ import (
 type logWriter struct{}
 
 func main() {
-	resp, err := http.Get("http://google.com")
-	if err != nil {
-		fmt.Println("error", err)
-		os.Exit(1)
-	}
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		resp, err := http.Get("http://golang.org/pkg")
+		if err != nil {
+			fmt.Println("error", err)
+			os.Exit(1)
+		}
 
-	lw := logWriter{}
-	io.Copy(lw, resp.Body)
+		io.Copy(w, resp.Body)
+	})
+	http.ListenAndServe(":8080", nil)
 }
 
 func (logWriter) Write(bs []byte) (int, error) {
