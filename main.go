87ac2f3f23d4c557e12a08f5e517d66a9f48a28c
Zobair Qauomi <Zobairq55@gmail.com>
2021-01-10 23:22:49 +0100

    add channels and go routines

--- main.go
+++ main.go
@@ -2,11 +2,48 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"net/http"
-	"os"
+	"time"
 )
 
+func main() {
+	links := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://golang.org",
+		"http://amazon.com",
+	}
+
+	c := make(chan string)
+
+	for _, link := range links {
+		go checkLink(link, c)
+	}
+
+	for l := range c {
+		go func(link string) {
+			time.Sleep(5 * time.Second)
+			checkLink(link, c)
+		}(l)
+	}
+
+}
+
+func checkLink(link string, c chan string) {
+	_, err := http.Get(link)
+
+	if err != nil {
+		fmt.Println(link, "might be down")
+		c <- link
+		return
+	}
+	fmt.Println(link, "is up!")
+	c <- link
+
+}
+
+/*
 type logWriter struct{}
 
 func main() {
@@ -27,3 +64,4 @@ func (logWriter) Write(bs []byte) (int, error) {
 
 	return len(bs), nil
 }
+*/
